b8f8a505ba4997b2adf259fe5bee4d1848c2d144
carlosdizx <carlodiaz@umariana.edu.co>
2021-06-22 16:29:20 -0500

    se valido el formulario de registro

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,32 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: "/about",
+    name: "About",
+    component: () => import("../views/About.vue")
   },
   {
-    path: '/editar/:id',
-    name: 'Editar',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Editar.vue')
+    path: "/editar/:id",
+    name: "Editar",
+    component: () => import("../views/Editar.vue")
+  },
+  {
+    path: "/registro",
+    name: "Registro",
+    component: () => import("../views/Registro.vue")
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
-export default router
+export default router;
---
+++ src/views/Registro.vue
@@ -0,0 +1,49 @@
+<template>
+  <h1>Formulario</h1>
+  <form class="my-5">
+    <input
+      v-model.trim="email"
+      class="form-control my-2"
+      type="email"
+      placeholder="Email"
+    />
+    <input
+      v-model.trim="password1"
+      class="form-control my-2"
+      type="password"
+      placeholder="Contraseña"
+    />
+    <input
+      class="form-control my-2"
+      type="password"
+      placeholder="Confirme contraseña"
+      v-model.trim="password2"
+    />
+    <button class="btn btn-success" type="submit" :disabled="bloquearBoton">
+      Registrar
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "Registro",
+  data: () => ({
+    email: "",
+    password1: "",
+    password2: ""
+  }),
+  computed: {
+    bloquearBoton() {
+      return (
+        !this.email.includes("@") ||
+        this.password1.length < 6 ||
+        this.password2.length < 6 ||
+        this.password1 !== this.password2
+      );
+    }
+  }
+};
+</script>
+
+<style scoped></style>
