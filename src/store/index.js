1930bae4b4468fb5040d80074c5d3177cecbdc0c
carlosdizx <carlodiaz@umariana.edu.co>
2021-06-23 09:36:44 -0500

    se creo un nuevo state para el error, y se muestra en la interfaz

--- src/store/index.js
+++ src/store/index.js
@@ -1,162 +1,186 @@
-import { createStore } from 'vuex'
-import router from '../router'
+import { createStore } from "vuex";
+import router from "../router";
 
 export default createStore({
   state: {
     tareas: [],
     tarea: {
-      id: '',
-      nombre: '',
+      id: "",
+      nombre: "",
       categorias: [],
-      estado: '',
+      estado: "",
       numero: 0
     },
-    user: null
+    user: null,
+    error: { tipo: null, message: "" }
   },
   mutations: {
+    setError(state, payload) {
+      if (payload === "EMAIL_NOT_FOUND") {
+        return (state.error = {
+          tipo: "email",
+          message: "Email no registrado"
+        });
+      }
+    },
     setUser(state, payload) {
-      state.user = payload
+      state.user = payload;
     },
     cargar(state, payload) {
-      state.tareas = payload
+      state.tareas = payload;
     },
     set(state, payload) {
-      state.tareas.push(payload)
+      state.tareas.push(payload);
     },
     eliminar(state, payload) {
-      state.tareas = state.tareas.filter(item => item.id !== payload)
+      state.tareas = state.tareas.filter(item => item.id !== payload);
     },
     tarea(state, payload) {
       if (!state.tareas.find(item => item.id === payload)) {
-        router.push('/')
-        return
+        router.push("/");
+        return;
       }
-      state.tarea = state.tareas.find(item => item.id === payload)
+      state.tarea = state.tareas.find(item => item.id === payload);
     },
     update(state, payload) {
-      state.tareas = state.tareas.map(item => item.id === payload.id ? payload : item)
-      router.push('/')
+      state.tareas = state.tareas.map(item =>
+        item.id === payload.id ? payload : item
+      );
+      router.push("/");
     }
   },
   actions: {
     cerrarSesion({ commit }) {
-      commit('setUser', null)
-      router.push('/ingreso')
-      localStorage.removeItem('usuario')
+      commit("setUser", null);
+      router.push("/ingreso");
+      localStorage.removeItem("usuario");
     },
     async ingresoUsuario({ commit }, usuario) {
       try {
-        const res = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDAVDt38rRKtPm-6q9S_H2v0Gq7u_V_wbY', {
-          method: 'POST',
-          body: JSON.stringify({
-            email: usuario.email,
-            password: usuario.password,
-            returnSecureToken: true
-          })
-        })
-        const userDB = await res.json()
-        console.log('userDB', userDB)
+        const res = await fetch(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDAVDt38rRKtPm-6q9S_H2v0Gq7u_V_wbY",
+          {
+            method: "POST",
+            body: JSON.stringify({
+              email: usuario.email,
+              password: usuario.password,
+              returnSecureToken: true
+            })
+          }
+        );
+        const userDB = await res.json();
         if (userDB.error) {
-          return console.log(userDB.error)
+          return commit("setError", userDB.error.message);
         }
-        commit('setUser', userDB)
-        router.push('/')
-        localStorage.setItem('usuario', JSON.stringify(userDB))
+        commit("setUser", userDB);
+        router.push("/");
+        localStorage.setItem("usuario", JSON.stringify(userDB));
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     },
     async registrarUsuario({ commit }, usuario) {
       try {
-        const res = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDAVDt38rRKtPm-6q9S_H2v0Gq7u_V_wbY', {
-          method: 'POST',
-          body: JSON.stringify({
-            email: usuario.email,
-            password: usuario.password,
-            returnSecureToken: true
-          })
-        })
-        const userDB = await res.json()
-        console.log(userDB)
+        const res = await fetch(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDAVDt38rRKtPm-6q9S_H2v0Gq7u_V_wbY",
+          {
+            method: "POST",
+            body: JSON.stringify({
+              email: usuario.email,
+              password: usuario.password,
+              returnSecureToken: true
+            })
+          }
+        );
+        const userDB = await res.json();
+        console.log(userDB);
         if (userDB.error) {
-          console.log(userDB.error)
-          return
+          console.log(userDB.error);
+          return;
         }
-        commit('setUser', userDB)
-        await router.push('/')
-        localStorage.setItem('usuario', JSON.stringify(userDB))
+        commit("setUser", userDB);
+        await router.push("/");
+        localStorage.setItem("usuario", JSON.stringify(userDB));
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     },
     async cargarLocalStorage({ commit, state }) {
-      if (localStorage.getItem('usuario')) {
-        commit('setUser', JSON.parse(localStorage.getItem('usuario')))
+      if (localStorage.getItem("usuario")) {
+        commit("setUser", JSON.parse(localStorage.getItem("usuario")));
       } else {
-        return commit('setUser', null)
+        return commit("setUser", null);
       }
       try {
-        const res = await fetch(`https://udemy-api-arena-default-rtdb.firebaseio.com/tareas/${state.user.localId}.json?auth=${state.user.idToken}`)
-        const dataDB = await res.json()
-        const arrayTareas = []
-        for (let id in dataDB){
-          arrayTareas.push(dataDB[id])
+        const res = await fetch(
+          `https://udemy-api-arena-default-rtdb.firebaseio.com/tareas/${state.user.localId}.json?auth=${state.user.idToken}`
+        );
+        const dataDB = await res.json();
+        const arrayTareas = [];
+        for (let id in dataDB) {
+          arrayTareas.push(dataDB[id]);
         }
-        commit('cargar', arrayTareas)
-
+        commit("cargar", arrayTareas);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     },
     async setTareas({ commit, state }, tarea) {
       try {
-        const res = await fetch(`https://udemy-api-arena-default-rtdb.firebaseio.com/tareas/${state.user.localId}/${tarea.id}.json?auth=${state.user.idToken}`, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(tarea)
-        })
+        const res = await fetch(
+          `https://udemy-api-arena-default-rtdb.firebaseio.com/tareas/${state.user.localId}/${tarea.id}.json?auth=${state.user.idToken}`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(tarea)
+          }
+        );
 
-        const dataDB = await res.json()
-        console.log(dataDB)
-
+        const dataDB = await res.json();
+        console.log(dataDB);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-      commit('set', tarea)
+      commit("set", tarea);
     },
     async deleteTareas({ commit, state }, id) {
       try {
-        await fetch(`https://udemy-api-arena-default-rtdb.firebaseio.com/tareas/${state.user.localId}/${id}.json?auth=${state.user.idToken}`, {
-          method: 'DELETE',
-        })
-        commit('eliminar', id)
+        await fetch(
+          `https://udemy-api-arena-default-rtdb.firebaseio.com/tareas/${state.user.localId}/${id}.json?auth=${state.user.idToken}`,
+          {
+            method: "DELETE"
+          }
+        );
+        commit("eliminar", id);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     },
     setTarea({ commit }, id) {
-      commit('tarea', id)
+      commit("tarea", id);
     },
     async updateTarea({ commit, state }, tarea) {
       try {
-        const res = await fetch(`https://udemy-api-arena-default-rtdb.firebaseio.com/tareas/${state.user.localId}/${tarea.id}.json?auth=${state.user.idToken}`, {
-          method: 'PATCH',
-          body: JSON.stringify(tarea)
-        })
-        const dataDB = await res.json()
-        commit('update', dataDB)
+        const res = await fetch(
+          `https://udemy-api-arena-default-rtdb.firebaseio.com/tareas/${state.user.localId}/${tarea.id}.json?auth=${state.user.idToken}`,
+          {
+            method: "PATCH",
+            body: JSON.stringify(tarea)
+          }
+        );
+        const dataDB = await res.json();
+        commit("update", dataDB);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     }
   },
   getters: {
     usuarioAutenticado(state) {
-      return !!state.user
+      return !!state.user;
     }
   },
-  modules: {
-  }
-})
+  modules: {}
+});
--- src/views/Ingreso.vue
+++ src/views/Ingreso.vue
@@ -1,55 +1,52 @@
 <template>
   <h1 class="my-5">Ingreso de Usuarios</h1>
+  <div class="alert alert-danger" v-if="error.tipo !== null">
+    {{ error.message }}
+  </div>
   <form @submit.prevent="procesarFormulario">
-        <input 
-            type="email" 
-            placeholder="email"
-            class="form-control my-2"
-            v-model.trim="email"
-        >
-        <input 
-            type="password" 
-            placeholder="password"
-            class="form-control my-2"
-            v-model.trim="pass1"
-        >
-        <button 
-            type="submit"
-            class="btn btn-primary"
-            :disabled="bloquear"
-        >
-        Ingresar
-        </button>
+    <input
+      type="email"
+      placeholder="email"
+      class="form-control my-2"
+      v-model.trim="email"
+    />
+    <input
+      type="password"
+      placeholder="password"
+      class="form-control my-2"
+      v-model.trim="pass1"
+    />
+    <button type="submit" class="btn btn-primary" :disabled="bloquear">
+      Ingresar
+    </button>
   </form>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapState } from "vuex";
 export default {
-    data() {
-        return {
-            email: '',
-            pass1: '',
-        }
-    },
-    computed: {
-        bloquear(){
-            if(!this.email.includes('@')){
-                return true
-            }
-            if(this.pass1.length > 5){
-                return false
-            }
-            return true
-        }
-    },
-    methods: {
-        ...mapActions(['ingresoUsuario']),
-        procesarFormulario(){
-            this.ingresoUsuario({email: this.email, password: this.pass1})
-            this.email = '';
-            this.pass1 = '';
-        }
+  data() {
+    return {
+      email: "",
+      pass1: ""
+    };
+  },
+  computed: {
+    ...mapState(["error"]),
+    bloquear() {
+      if (!this.email.includes("@")) {
+        return true;
+      }
+      return this.pass1.length <= 5;
     }
-}
+  },
+  methods: {
+    ...mapActions(["ingresoUsuario"]),
+    procesarFormulario() {
+      this.ingresoUsuario({ email: this.email, password: this.pass1 });
+      this.email = "";
+      this.pass1 = "";
+    }
+  }
+};
 </script>
